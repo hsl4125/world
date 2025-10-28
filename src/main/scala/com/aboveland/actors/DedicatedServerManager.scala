aae41ce121bb86bbcef3041e5f87981e89971705
huangh <hsl4125@outlook.com>
2025-10-28 19:08:29 +0800

    feature: UpdateServer

--- src/main/scala/com/aboveland/actors/DedicatedServerManager.scala
+++ src/main/scala/com/aboveland/actors/DedicatedServerManager.scala
@@ -2,7 +2,8 @@ package com.aboveland.actors
 
 import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
-import com.aboveland.models.BaseServer
+import com.aboveland.actors.DedicatedServer.UpdateServer
+import com.aboveland.models.{BaseServer, BaseServerStatus}
 
 object DedicatedServerManager {
 
@@ -14,15 +15,40 @@ object DedicatedServerManager {
 
   def apply(): Behavior[Command] = Behaviors.setup { context =>
     context.log.info("DedicatedServerManager started")
-    
-    Behaviors.receive { (ctx, cmd) =>
-      cmd match {
-        case RegisterServer(server, replyTo) =>
-          ctx.log.info("RegisterServer: {}", server)
-          ctx.spawn(DedicatedServer(server), s"dedicated-server-${server.sid}")
-          replyTo ! RegisterServerResponse(server)
-          Behaviors.same
-      }
+    run(0L) // Start with maxServerID = 0
+  }
+
+  private def run(maxServerID: Long): Behavior[Command] = Behaviors.receive { (ctx, cmd) =>
+    cmd match {
+      case RegisterServer(server, replyTo) =>
+        ctx.child(makeServerName(server)) match {
+          case Some(serverRef) =>
+            ctx.log.info("RegisterServer need update: {}", server)
+            serverRef.asInstanceOf[ActorRef[DedicatedServer.Command]] ! UpdateServer(server, replyTo)
+            // TODO: [error] match may not be exhaustive. case: Some(_)
+            // serverRef ! UpdateServer(server, replyTo)
+            run(maxServerID)
+          case None =>
+            // Increment maxServerID and assign it to the new server
+            val newServerID = maxServerID + 1
+            val updatedServer = server.copy(sid = newServerID, status = BaseServerStatus.STARTING)
+
+            ctx.log.info("RegisterServer: {} with assigned ID: {}", server.machineId, newServerID)
+
+            // Create DedicatedServer actor with the updated server (new sid)
+            val serverName = makeServerName(updatedServer)
+            ctx.spawn(DedicatedServer(updatedServer), serverName)
+
+            // Reply with the updated server
+            replyTo ! RegisterServerResponse(updatedServer)
+
+            // Continue with updated maxServerID state
+            run(newServerID)
+        }
     }
+  }
+
+  private def makeServerName(server: BaseServer): String = {
+    s"ds-${server.machineId}-${server.port}"
   }
 }
